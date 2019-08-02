c9f751b2fa106e424eef0a9f80a74c19c56c1c03
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-08-02 14:50:56 +0300

    init

---
+++ src/test/java/org/csystem/springtempapp/SpringTempAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csystem.springtempapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTempAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
